7ba8cb30a7032e37e29d728954a562cedfbc1ddb
Jasper <80272820+Code-Jap@users.noreply.github.com>
2022-02-13 17:50:37 +0100

    Add files via upload

--- api-client.js
+++ api-client.js
@@ -1,15 +1,24 @@
 const API_KEY = "e27d8c517052a801f9f9831a71afa2b8";
+const externalSource = "imdb_id";
+const genre = '18';
+const year = 1975;
 
-const getData = async () => {
+const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+const favouriteMovieUrl = `https://api.themoviedb.org/3/find/tt0245429?api_key=${API_KEY}&external_source=${externalSource}`;
+const topTenUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
+const topActionUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genre}`
+const moviesFrom1975Url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&year=${year}`
+
+
+const getData = async (url) => {
   try {
-    const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
-    const res = await fetch(apiUrl);
+    // const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+    const res = await fetch(url);
     const data = await res.json();
-  // console.log(res)
-     console.log(data);
-
+    
     return data;
   } catch (err) {
-    console.log(errmessage);
+    console.log(err.message);
   }
 };
+
--- script.js
+++ script.js
@@ -1,26 +1,131 @@
-const unorderedList = document.querySelector("#ul");
-
-const retrieveData = async () => {
-  await getData();
-};
+const unorderedList1 = document.querySelector("#ul");
+const unorderedList2 = document.querySelector("#ul2");
+const unorderedList3 = document.querySelector("#ul3");
+const unorderedList4 = document.querySelector("#ul4");
+const favourite = document.querySelector("#favourite");
 
 
 const getMovieGenres = async (data) => {
+  const dataObject = await data;
+  console.log("Dataobject:", dataObject);
+
+  const genreArray = dataObject.genres;
+  console.log("GenreArray:", genreArray);
+
   try {
-    const listItems = await data.map((item) => {
-      
+    const listItems = genreArray.map((item) => {
       const newLi = document.createElement("li");
+      const liString = `Genre naam: ${item.name}, id: ${item.id}`;
+
+      newLi.innerHTML = liString;
       return newLi;
     });
-  
-    listItems.forEach((li) => {
-      unorderedList.appendChild(li);
-    });
 
+    listItems.forEach((item) => {
+      unorderedList1.appendChild(item);
+    });
   } catch (err) {
-    console.log(err.message)
+    console.log(err.message);
   }
-  
 };
 
-getMovieGenres(retrieveData());
+getMovieGenres(getData(apiUrl));
+
+const GetSpiritedAway = async (data) => {
+  const dataObject = await data;
+  console.log("getSpiritedAway dataObject:", dataObject);
+
+  const results = dataObject.movie_results;
+  console.log("getSpiritedAway results:", results);
+
+  const title = results[0].title;
+  console.log("getSpiritedAway title:", title);
+
+  try {
+    const movieName = title;
+    favourite.innerHTML = movieName;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+GetSpiritedAway(getData(favouriteMovieUrl));
+
+const getTopRatedMovies = async (data) => {
+  const dataObject = await data;
+  console.log("getTopRatedMovies dataObject:", dataObject);
+
+  const results = dataObject.results;
+  console.log("getTopRatedMovies results:", results);
+
+  try {
+    const listItems = results.slice(0, 10).map((item) => {
+      const newLi = document.createElement("li");
+      const liString = item.title;
+
+      newLi.innerHTML = liString;
+      return newLi;
+    });
+
+    listItems.forEach((item) => {
+      unorderedList2.appendChild(item);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+getTopRatedMovies(getData(topTenUrl));
+
+const getTopRatedActionMovies = async (data) => {
+  const dataObject = await data;
+  console.log("getTopRAtedActionMovie dataObject:", dataObject);
+
+  const results = dataObject.results;
+  console.log("getTopRatedActioMovie results:", results);
+
+  try {
+    const listItems = results.slice(0, 10).map((item) => {
+      const newLi = document.createElement("li");
+      const liString = item.title;
+
+      newLi.innerHTML = liString;
+      return newLi;
+    });
+
+    listItems.forEach((item) => {
+      unorderedList3.appendChild(item);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+getTopRatedActionMovies(getData(topActionUrl));
+
+
+const getMoviesFrom1975 = async (data) => {
+  const dataObject = await data;
+  console.log("getMoviesFrom1975 dataObject:", dataObject);
+
+  const results = dataObject.results;
+  console.log("getMoviesFrom1975 results:", results);
+
+  try {
+    const listItems = results.slice(0, 10).map((item) => {
+      const newLi = document.createElement("li");
+      const liString = item.title;
+
+      newLi.innerHTML = liString;
+      return newLi;
+    });
+
+    listItems.forEach((item) => {
+      unorderedList4.appendChild(item);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+getMoviesFrom1975(getData(moviesFrom1975Url));
